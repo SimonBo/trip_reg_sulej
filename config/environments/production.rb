ea8ee7efaba0683c238a93487c8c56822f8dc172
SimonBo <szymon.borucki@gmail.com>
2018-05-06 14:17:54 +0200

    setup mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,13 +74,11 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "umsulejowek.home.pl",
     port: 587,
-    domain: Rails.application.secrets.domain_name,
     authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
+    user_name: 'arosa@umsulejowek.pl',
+    password: 'Arosa1O@'
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
