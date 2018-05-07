0bac3ef70d987add7dda7911901d29e81e63036b
SimonBo <szymon.borucki@gmail.com>
2018-05-07 15:07:34 +0200

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.10.2"
 set :application, 'trip_reg_sulej'
 set :repo_url, 'git@github.com:SimonBo/trip_reg_sulej.git'  # Edit this to match your repository
 set :branch, :master
-set :deploy_to, '/home/deploy/my-app'
+set :deploy_to, '/home/deploy/trip_reg_sulej'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
