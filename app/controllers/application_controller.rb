3c22f1a1cdfdf44ab786a149fef687706a7a0dbe
szymon.borucki@gmail.com <szymon.borucki@gmail.com>
2019-05-09 14:19:01 +0200

    auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # http_basic_authenticate_with name: ENV['auth_name'], password: ENV['auth_pass']
+  http_basic_authenticate_with name: ENV['auth_name'], password: ENV['auth_pass']
 end
